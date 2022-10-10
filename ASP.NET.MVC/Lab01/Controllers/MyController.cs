9ab697bdc69135a6942285626d31e7b508f4451a
IvanovVS1988 <103385511+IvanovVS1988@users.noreply.github.com>
2022-10-10 18:31:03 +0300

    Add files via upload

---
+++ ASP.NET.MVC/Lab01/Controllers/MyController.cs
@@ -0,0 +1,25 @@
+﻿using Lab01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class MyController : Controller
+    {
+        /*
+        public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }
+    }
+}
